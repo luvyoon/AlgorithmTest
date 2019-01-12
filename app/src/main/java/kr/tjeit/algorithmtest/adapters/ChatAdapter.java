678083d42253b52579ada22d5ef2cc999f8cd93a
Hottory <gkffndnl123@naver.com>
2019-01-12 14:11:30 +0900

    채팅 어댑터 데이터 출력 마무리

--- app/src/main/java/kr/tjeit/algorithmtest/adapters/ChatAdapter.java
+++ app/src/main/java/kr/tjeit/algorithmtest/adapters/ChatAdapter.java
@@ -1,10 +1,13 @@
 package kr.tjeit.algorithmtest.adapters;
 
 import android.content.Context;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import java.io.Serializable;
 import java.util.List;
@@ -38,6 +41,40 @@ public class ChatAdapter extends ArrayAdapter<Chat> {
 
             row = inf.inflate(R.layout.chat_list_item,null);
         }
+
+        LinearLayout computerMsgLayout = row.findViewById(R.id.computerMsgLayout);
+        TextView computerMsgTxt = row.findViewById(R.id.computerMsgTxt);
+        LinearLayout userMsgLayout = row.findViewById(R.id.userMsgLayout);
+        TextView userMsgTxt = row.findViewById(R.id.userMsgTxt);
+
+
+        Chat data = mList.get(position);
+
+//        data가 가진 sender를 봤을때 computer /user 두가지 경우가 존재.
+
+
+        if (data.getSender().equals("user")){
+//            사용자가 입력한 메세지
+
+            computerMsgLayout.setVisibility(View.GONE);
+            userMsgLayout.setVisibility(View.VISIBLE);
+
+            userMsgTxt.setText(data.getSender());
+
+
+
+        }else{
+//            컴퓨터가 입력한 메세지
+
+            userMsgLayout.setVisibility(View.GONE);
+            computerMsgLayout.setVisibility(View.VISIBLE);
+
+            computerMsgTxt.setText(data.getSender());
+
+
+        }
+
+
 
         return row;
     }
